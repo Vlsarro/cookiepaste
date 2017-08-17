74b41d61de18e6df39df347419e960d368fe4420
Vlsarro <drekmaro@gmail.com>
2017-08-17 08:17:14 +0300

    Add function to show information messages

--- popup.js
+++ popup.js
@@ -5,18 +5,21 @@ document.addEventListener('DOMContentLoaded', function() {
 		cookieKey = document.getElementById('cookie-key'),
 		resultDiv = document.getElementById('result');
 
+	function infoDiv(div, text) {
+		div.textContent = text;
+		div.style.display = 'block';
+	}
+
 	replaceButton.addEventListener('click', function(e) {
 		e.preventDefault();
 		browser.cookies.get({url: firstDomain.value, name: cookieKey.value}).then(function (cookie) {
 			var resultText;
 			browser.cookies.set({url: secondDomain.value, name: cookieKey.value, value: cookie.value}).then(function (cookie) {
 				resultText = 'Cookie ' + cookie.name + ' of ' + secondDomain.value  + ' has been replaced with ' + cookie.value;
-				resultDiv.appendChild(document.createTextNode(resultText));
-				resultDiv.style.display = 'block';
+				infoDiv(resultDiv, resultText);
 			}).fail(function (err) {
 				resultText = 'Error. Cookie value hasn\'t been set';
-				resultDiv.appendChild(document.createTextNode(resultText));
-				resultDiv.style.display = 'block';
+				infoDiv(resultDiv, resultText);
 			});
 		}).fail(function (err) {
 			console.error('No cookie for you');
